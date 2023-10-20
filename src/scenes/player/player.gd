ab8125e23978c06cbba0a570718dea0163c27f07
Luc4sKr <lucaskreuch77@gmail.com>
2023-10-19 21:03:42 -0300

    player animation

--- src/scenes/player/player.gd
+++ src/scenes/player/player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 
+@onready var animation: AnimatedSprite2D = $"animation"
+
 var speed = 300
 
 func _physics_process(delta: float) -> void:
 	self.move()
+	self.animate()
+	self.verify_direction()
 	
 func move() -> void:
 	var direction: Vector2 = Vector2(
@@ -12,7 +16,17 @@ func move() -> void:
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	).normalized()
 	
-	print(direction)
-	
-	velocity = direction * speed
+	self.velocity = direction * speed
 	move_and_slide()
+	
+func animate() -> void:
+	if self.velocity != Vector2.ZERO:
+		self.animation.play("run")
+	else:
+		self.animation.play("idle")
+
+func verify_direction() -> void:
+	if self.velocity.x > 0:
+		self.animation.flip_h = false
+	if self.velocity.x < 0:
+		self.animation.flip_h = true
